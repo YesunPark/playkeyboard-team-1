9c45e9b4963a8078463fc8fffede5b8d0564c195
lkw1996 <lkw3703@gmail.com>
2022-10-05 15:14:13 +0900

    [Add] 테마 레이아웃 추가

--- src/pages/Category/CategoryTheme.js
+++ src/pages/Category/CategoryTheme.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
 
 const CategoryTheme = () => {
@@ -12,9 +13,22 @@ const CategoryTheme = () => {
     getList();
   }, []);
 
-  console.log('List :', list);
-
-  return;
+  return (
+    <>
+      {list.length !== 0 &&
+        list.data.data.map((data) => {
+          return (
+            <div className='theme-container'>
+              <img src={data.imageUrl} alt='image' className='theme-img' />
+              <span className='theme-name'>{data.name}</span>
+              <span className='theme-hashtag'>{data.hashtag}</span>
+              <span className='theme-download'>{data.downloads}</span>
+              <span className='theme-price'>{data.price}</span>
+            </div>
+          );
+        })}
+    </>
+  );
 };
 
 export default CategoryTheme;
