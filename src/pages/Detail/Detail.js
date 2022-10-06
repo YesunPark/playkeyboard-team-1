a50d76ea110220852f7d999a5139333f105dc779
jeongyeon <jeongyeon@MacBookPro.local>
2022-10-06 20:03:05 +0900

    [Add] 상품 문의 버튼 UI

---
+++ src/pages/Detail/Detail.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import styled from 'styled-components';
+import QnaButton from './QnaButton';
+import BuyButton from './BuyButton';
+import DetailIcon from './DetailIcon';
+
+const Detail = () => {
+  const params = useParams();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios(`https://api.plkey.app/theme/${params.themeId}`).then((res) => {
+      setData(res.data.data);
+    });
+  }, [params]);
+
+  return (
+    <>
+      {data && (
+        <DetailContainer>
+          <div className='detail-container'>
+            <DetailIcon />
+            <QnaButton />
+            <BuyButton data={data} />
+          </div>
+        </DetailContainer>
+      )}
+    </>
+  );
+};
+
+const DetailContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .detail-container {
+    width: 640px;
+    min-height: 100vh;
+    border: 1px solid black;
+  }
+
+  @media ${({ theme }) => theme.responsive.tablet} {
+    .detail-container {
+      width: 100%;
+    }
+  }
+`;
+
+export default Detail;
