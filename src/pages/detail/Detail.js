f08a12d32065b5e4837560380630b47c29a0df2d
YesunPark <lynn08082@gmail.com>
2022-10-06 21:33:58 +0900

    [Del] 겹치는 import삭제

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -2,8 +2,6 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-import styled from 'styled-components';
-
 import DetailInfo from './DetailInfo';
 import DetailEmoji from './DetailEmoji';
 import DetailBuyButton from './DetailBuyButton';
