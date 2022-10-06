803a3f910eacec4f80cbb426f919259ecf618c18
YesunPark <lynn08082@gmail.com>
2022-10-06 16:43:49 +0900

    [Fix] 라우터 메인페이지 변경

--- src/Router.js
+++ src/Router.js
@@ -12,7 +12,7 @@ function Router() {
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Routes>
-          <Route path={`${process.env.PUBLIC_URL}/`} element={<Category />} />
+          <Route path='/' element={<Category />} />
           <Route path='/detail/:themeId' element={<Detail />} />
         </Routes>
       </ThemeProvider>
